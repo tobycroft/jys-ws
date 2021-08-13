854e3025d0e498591e8ec90572c72fa7b0b1b50c
火线兔 <silverhawk@foxmail.com>
2021-08-13 11:57:08 +0800

    TuuzPC

--- api/pushmsg.go
+++ api/pushmsg.go
@@ -1,42 +1,11 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/tidwall/gjson"
 	"main.go/function/ws"
 )
-
-func Pushmsg(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Headers", "Content-Type")
-	c.Header("content-type", "application/json")
-	body, _ := c.GetRawData()
-	//fmt.Println(time.Now().Local().Format("2006-01-02 15:04:05"),r.URL,message)
-	p := ws.NewMsgPack()
-	err := json.Unmarshal(body, &p)
-	if err != nil {
-		fmt.Println("拆解推送数据包失败:", err.Error())
-		c.String(200, "error")
-		return
-	}
-	if p.SocketType == "" {
-		fmt.Println("推送数据包没有类型")
-		c.String(200, "error")
-		return
-	}
-	//go func() {
-	//	//根据消息类型，向指定订阅队列发广播
-	//	for _, queue := range h.UserQueue {
-	//		if queue.SubscribeType == p.SocketType {
-	//			queue.Broadcast <- []byte(message)
-	//		}
-	//	}
-	//}()
-	c.String(200, "ok")
-	return
-}
 
 func PushmsgArray(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
@@ -52,13 +21,6 @@ func PushmsgArray(c *gin.Context) {
 		return
 	}
 	go func(data gjson.Result) {
-		//defer func() {
-		//	if r := recover(); r != nil {
-		//		fmt.Printf("捕获到的错误：%s\n", r)
-		//	}
-		//}()
-		//根据消息类型，向指定订阅队列发广播
-
 		for _, result := range data.Array() {
 			if !result.Get("socket_type").Exists() {
 				continue
