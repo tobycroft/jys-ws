93f055b282af5b159f85fda9fc53ad37b3685f80
火线兔 <silverhawk@foxmail.com>
2021-08-13 15:06:01 +0800

    TuuzPC

--- function/ws/out.go
+++ function/ws/out.go
@@ -1,8 +1,16 @@
 package ws
 
-import "github.com/gorilla/websocket"
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+)
 
 func UserMsgChan(conn *websocket.Conn) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("捕获到的错误：%s\n", r)
+		}
+	}()
 	cc, has := Conn2Chan.Load(conn)
 	if has {
 		for data := range cc.(chan string) {
